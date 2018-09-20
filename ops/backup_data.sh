99addeaef09871e731c03e48ec3145cd1e069cfa
Bjornn Borg <bjornnborg@gmail.com>
2018-09-20 02:52:14 -0300

    Fixing path to save backup

--- ops/backup_data.sh
+++ ops/backup_data.sh
@@ -1,6 +1,6 @@
 if [ -z "$1" ]
   then
-    echo "No backup path provided. Usage: ./ops/backup_data some/path/to/save/"
+    echo "No backup path provided. Usage: ./ops/backup_data some/path/to/save/ with trailing slash"
     exit 1
 fi
-docker exec bosc-mysql /usr/bin/mysqldump -u root --password=root_pass budget-on-sight_development > backup-client-$(date +"%d_%m_%Y@%H:%M:%S").sql;
+docker exec bosc-mysql /usr/bin/mysqldump -u root --password=root_pass budget-on-sight_development > $1backup-client-$(date +"%d_%m_%Y@%H:%M:%S").sql;
